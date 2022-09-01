3d9d15356b6e9bd359b75473563f9d7775c59b42
phillip burns <phillipworld@hotmail.co.uk>
2022-09-01 23:47:27 +0100

    Added mongo database for storage

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,10 +3,24 @@ import { fileURLToPath } from "url";
 import { dirname, join as pathJoin } from "path";
 import bodyParser from "body-parser";
 import lodash from "lodash";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = dirname(__filename);
+const database: string = "journalDB";
 
+await mongoose
+  .connect(
+    `mongodb+srv://shushyy:${process.env.MONGO}@cluster0.szrpyuj.mongodb.net/${database}`
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app: express.Application = express();
 const port: any = process.env.PORT || 8080;
@@ -15,8 +29,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.listen(port);
+app.locals.lodash = lodash;
 
-const posts: any[] = [];
+const articleSchema = new mongoose.Schema({
+  title: {
+    require: true,
+    type: String,
+  },
+  body: String,
+});
+
+const Article = mongoose.model("Article", articleSchema);
 
 const homeStartingContent: String =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,7 +49,16 @@ const contactContent: String =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get("/", (req: Request, res: Response) => {
-  res.render("home", { homeContent: homeStartingContent, posts: posts });
+  Article.find({}, (err: any, foundArticles: any) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        homeContent: homeStartingContent,
+        posts: foundArticles,
+      });
+    }
+  });
 });
 
 app.get("/about", (req: Request, res: Response) => {
@@ -42,18 +74,17 @@ app.get("/compose", (req: Request, res: Response) => {
 });
 
 app.post("/compose", (req: Request, res: Response) => {
-  const post = {
-    title: req.body.postTitle,
+  const post = new Article({
+    title: lodash.kebabCase(req.body.postTitle),
     body: req.body.postBody,
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save().then(() => res.redirect("/"));
 });
 
 app.get("/posts/:postID", (req: Request, res: Response) => {
-
   const requestedPost = req.params.postID;
-  const {title, body} = posts.find(post => lodash.lowerCase(post.title) === lodash.lowerCase(requestedPost));
 
-  res.render("post", {postData: {title, body}});
-})
+  Article.findOne({ title: requestedPost }).then((post: any) => {
+    res.render("post", { postData: { title: post.title, body: post.body } });
+  });
+});
